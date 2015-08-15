7642df9ea4650ba17de21ffe1e17664c418d4d1f
witspirit <witspirit@gmail.com>
2015-08-15 09:21:22 +0200

    A first tiny piece of logic...

--- src/main/java/be/witspirit/rfidtrialprocess/ParseInput.java
+++ src/main/java/be/witspirit/rfidtrialprocess/ParseInput.java
@@ -1,7 +1,38 @@
 package be.witspirit.rfidtrialprocess;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Reads the Input CSV
  */
 public class ParseInput {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ParseInput.class);
+
+    public List<RfidScan> readFrom(InputStream inputStream) throws IOException {
+        List<RfidScan> scans = new ArrayList<>();
+        InputStreamReader input = new InputStreamReader(inputStream);
+        CSVParser parser = CSVFormat.DEFAULT.withDelimiter(';').withHeader().parse(input);
+        for (CSVRecord record : parser.getRecords()) {
+            String nr = record.get("No");
+            String trType = record.get("TrType");
+            String uid = record.get("UID");
+            String time = record.get("Time");
+            String antNr = record.get("AntNo");
+
+            LOG.debug(nr+":"+trType+":"+uid+":"+time+":"+antNr);
+        }
+
+        return scans;
+    }
 }
--- src/main/java/be/witspirit/rfidtrialprocess/RfidScan.java
+++ src/main/java/be/witspirit/rfidtrialprocess/RfidScan.java
@@ -1,7 +1,59 @@
 package be.witspirit.rfidtrialprocess;
 
+import java.time.Instant;
+
 /**
  * Represents an individual RFID Scan line
  */
 public class RfidScan {
+
+    private int nr;
+
+    private int transponderType;
+
+    private byte[] uid;
+
+    private Instant timestamp;
+
+    private int antennaNr;
+
+    public int getNr() {
+        return nr;
+    }
+
+    public void setNr(int nr) {
+        this.nr = nr;
+    }
+
+    public int getTransponderType() {
+        return transponderType;
+    }
+
+    public void setTransponderType(int transponderType) {
+        this.transponderType = transponderType;
+    }
+
+    public byte[] getUid() {
+        return uid;
+    }
+
+    public void setUid(byte[] uid) {
+        this.uid = uid;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getAntennaNr() {
+        return antennaNr;
+    }
+
+    public void setAntennaNr(int antennaNr) {
+        this.antennaNr = antennaNr;
+    }
 }
